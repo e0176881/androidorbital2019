586f7be83e24f135458db4a44333a67163ad40c6
Zac Chan <cqhchan@gmail.com>
2019-05-09 16:45:57 +0800

    Add user details model

---
+++ app/src/main/java/com/example/orbital2019/intermediate/model/UserDetails.java
@@ -0,0 +1,59 @@
+package com.example.orbital2019.intermediate.model;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserDetails {
+
+    // Database Keys
+    public static final String userDetailsKey = "UserDetails";
+    public static final String nameKey = "Name";
+    public static final String matriculationNumberKey = "MatriculationNumber";
+
+
+    private String matriculationNumber;
+    public String name;
+
+
+    public UserDetails(String matriculationNumber , String name){
+
+        this.matriculationNumber = matriculationNumber;
+        this.name = name;
+
+    }
+
+    // call to update entry In database.
+    public void updateEntry () {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String,Object> data = new HashMap<>();
+
+        data.put(nameKey,name);
+        data.put(matriculationNumberKey,matriculationNumber);
+
+
+        db.collection(userDetailsKey).document(matriculationNumberKey).update(data);
+    }
+
+
+    // call to create entry In database.
+    public void createEntry () {
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String,Object> data = new HashMap<>();
+
+        data.put(nameKey,name);
+        data.put(matriculationNumberKey,matriculationNumber);
+
+
+        db.collection(userDetailsKey).document(matriculationNumberKey).set(data);
+    }
+
+
+
+}
