6e8ffc8d2c54ef2e1a89c33db849f1ac358d7b7a
Zac Chan <cqhchan@gmail.com>
2019-05-09 16:46:17 +0800

    Added create user details in register page

--- app/src/main/java/com/example/orbital2019/intermediate/auth/LoginFragment.java
+++ app/src/main/java/com/example/orbital2019/intermediate/auth/LoginFragment.java
@@ -1,6 +1,5 @@
-package com.example.orbital2019;
+package com.example.orbital2019.intermediate.auth;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -14,19 +13,25 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.example.orbital2019.intermediate.LoginFragment;
+
+import com.example.orbital2019.HomeFragment;
+import com.example.orbital2019.R;
+
+
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class RegisterFragment extends Fragment  {
+public class LoginFragment extends Fragment  {
 
     private EditText emailTV, passwordTV;
-    private Button regBtn;
+    private Button loginBtn;
     private ProgressBar progressBar;
 
     private FirebaseAuth mAuth;
+
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -36,23 +41,23 @@ public class RegisterFragment extends Fragment  {
 
         emailTV = getActivity().findViewById(R.id.email);
         passwordTV = getActivity().findViewById(R.id.password);
-        regBtn = getActivity().findViewById(R.id.register);
+
+        loginBtn = getActivity().findViewById(R.id.login);
         progressBar = getActivity().findViewById(R.id.progressBar);
 
-        regBtn.setOnClickListener(new View.OnClickListener() {
+
+
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                registerNewUser();
+                loginUserAccount();
             }
         });
 
 
-
-
-
     }
-
-    private void registerNewUser() {
+    private void loginUserAccount() {
         progressBar.setVisibility(View.VISIBLE);
 
         String email, password;
@@ -68,28 +73,27 @@ public class RegisterFragment extends Fragment  {
             return;
         }
 
-        mAuth.createUserWithEmailAndPassword(email, password)
+        mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(getActivity().getApplicationContext(), "Registration successful!", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getActivity().getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
                             progressBar.setVisibility(View.GONE);
 
                             FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
-                            ft.replace(R.id.content_frame, new LoginFragment());
+                            ft.replace(R.id.content_frame, new HomeFragment());
                             ft.commit();
                         }
                         else {
-                            Toast.makeText(getActivity().getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getActivity().getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
                             progressBar.setVisibility(View.GONE);
                         }
                     }
                 });
     }
 
-
-    public RegisterFragment() {
+    public LoginFragment() {
         // Required empty public constructor
     }
 
@@ -97,7 +101,7 @@ public class RegisterFragment extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.activity_register, container, false);
+        View view = inflater.inflate(R.layout.activity_login, container, false);
 
         return view;
     }
--- app/src/main/java/com/example/orbital2019/intermediate/auth/RegisterFragment.java
+++ app/src/main/java/com/example/orbital2019/intermediate/auth/RegisterFragment.java
@@ -1,10 +1,11 @@
-package com.example.orbital2019.intermediate;
+package com.example.orbital2019.intermediate.auth;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,25 +14,19 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-
-import com.example.orbital2019.HomeFragment;
 import com.example.orbital2019.R;
-
-
-
+import com.example.orbital2019.intermediate.model.UserDetails;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class LoginFragment extends Fragment  {
+public class RegisterFragment extends Fragment  {
 
-    private EditText emailTV, passwordTV;
-    private Button loginBtn;
+    private EditText emailTV, passwordTV, matriculationNumberTV, nameTV;
+    private Button regBtn;
     private ProgressBar progressBar;
-
     private FirebaseAuth mAuth;
-
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -41,28 +36,34 @@ public class LoginFragment extends Fragment  {
 
         emailTV = getActivity().findViewById(R.id.email);
         passwordTV = getActivity().findViewById(R.id.password);
+        matriculationNumberTV = getActivity().findViewById(R.id.matriculation_number);
+        nameTV = getActivity().findViewById(R.id.name);
 
-        loginBtn = getActivity().findViewById(R.id.login);
+        regBtn = getActivity().findViewById(R.id.register);
         progressBar = getActivity().findViewById(R.id.progressBar);
 
-
-
-
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        regBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                loginUserAccount();
+                registerNewUser();
             }
         });
 
 
+
+
+
     }
-    private void loginUserAccount() {
+
+    private void registerNewUser() {
         progressBar.setVisibility(View.VISIBLE);
 
-        String email, password;
+        final String email, password, matriculationNumber, name;
         email = emailTV.getText().toString();
         password = passwordTV.getText().toString();
+        matriculationNumber = emailTV.getText().toString();
+        name = passwordTV.getText().toString();
+
 
         if (TextUtils.isEmpty(email)) {
             Toast.makeText(getActivity().getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
@@ -73,27 +74,43 @@ public class LoginFragment extends Fragment  {
             return;
         }
 
-        mAuth.signInWithEmailAndPassword(email, password)
+        if (TextUtils.isEmpty(matriculationNumber)) {
+            Toast.makeText(getActivity().getApplicationContext(), "Please enter Matriculation Number...", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(name)) {
+            Toast.makeText(getActivity().getApplicationContext(), "Please enter name!", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
+
                         if (task.isSuccessful()) {
-                            Toast.makeText(getActivity().getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getActivity().getApplicationContext(), "Registration successful!", Toast.LENGTH_LONG).show();
                             progressBar.setVisibility(View.GONE);
 
+
+                            //Create user details if successful.
+                            UserDetails currentUser = new UserDetails(matriculationNumber, name);
+                            currentUser.createEntry();
+
                             FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
-                            ft.replace(R.id.content_frame, new HomeFragment());
+                            ft.replace(R.id.content_frame, new LoginFragment());
                             ft.commit();
                         }
                         else {
-                            Toast.makeText(getActivity().getApplicationContext(), "Login failed! Please try again later", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getActivity().getApplicationContext(), "Registration failed! Please try again later", Toast.LENGTH_LONG).show();
                             progressBar.setVisibility(View.GONE);
                         }
                     }
                 });
     }
 
-    public LoginFragment() {
+
+    public RegisterFragment() {
         // Required empty public constructor
     }
 
@@ -101,7 +118,7 @@ public class LoginFragment extends Fragment  {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.activity_login, container, false);
+        View view = inflater.inflate(R.layout.activity_register, container, false);
 
         return view;
     }
