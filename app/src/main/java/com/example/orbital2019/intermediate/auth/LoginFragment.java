ff4dfb0fedfbfdd7006513b6f6325035e9952a02
mingxuan <e0176881@u.nus.edu>
2019-05-10 23:54:29 +0800

    password reset for firebase auth

--- app/src/main/java/com/example/orbital2019/intermediate/auth/LoginFragment.java
+++ app/src/main/java/com/example/orbital2019/intermediate/auth/LoginFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -26,6 +29,7 @@ public class LoginFragment extends Fragment  {
     private EditText emailEditText, passwordEditText;
     private Button loginBtn;
     private ProgressBar progressBar;
+    private TextView forgotPw;
 
     private FirebaseAuth mAuth;
 
@@ -40,7 +44,7 @@ public class LoginFragment extends Fragment  {
 
         loginBtn = getActivity().findViewById(R.id.login);
         progressBar = getActivity().findViewById(R.id.progressBar);
-
+        forgotPw = getActivity().findViewById(R.id.tvforgotpw);
 
 
 
@@ -51,8 +55,22 @@ public class LoginFragment extends Fragment  {
             }
         });
 
+        forgotPw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                ft.replace(R.id.content_frame, new ResetPasswordFragment());
+                ft.commit();
+            }
+        });
+
 
     }
+
+
+
+
     private void loginUserAccount() {
         progressBar.setVisibility(View.VISIBLE);
 
---
+++ app/src/main/java/com/example/orbital2019/intermediate/auth/ResetPasswordFragment.java
@@ -0,0 +1,92 @@
+package com.example.orbital2019.intermediate.auth;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.example.orbital2019.HomeFragment;
+import com.example.orbital2019.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetPasswordFragment extends Fragment {
+
+    private EditText emailEditText;
+    private Button resetBtn;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailEditText = getActivity().findViewById(R.id.txtemail);
+
+        resetBtn = getActivity().findViewById(R.id.btnresetpassword);
+        progressBar = getActivity().findViewById(R.id.resetprogressBar);
+
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetUserAccount();
+            }
+        });
+
+
+    }
+
+    private void resetUserAccount() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        String email;
+        email = emailEditText.getText().toString();
+
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getActivity().getApplicationContext(), "Please enter email...", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+
+
+        mAuth.sendPasswordResetEmail(email)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getActivity().getApplicationContext(), "New password sent to email!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    public ResetPasswordFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_resetpassword, container, false);
+
+        return view;
+    }
+
+
+}
